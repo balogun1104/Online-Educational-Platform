728a103013c026c1429f620758874bc25dd5c801
Balogun Razak <razak2free@gmail.com>
2023-03-04 06:56:28 +0100

    changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -52,7 +52,7 @@ const Footer = () => {
         <Row>
           <Col lg="3" md="6" className="mb-4">
             <h2 className=" d-flex align-items-center gap-1">
-              <i class="ri-pantone-line"></i> Learners.
+              <i class="ri-pantone-line"></i> E-LNOTE.
             </h2>
 
             <div className="follows">
@@ -114,8 +114,8 @@ const Footer = () => {
           <Col lg="3" md="6">
             <h6 className="fw-bold">Get in Touch</h6>
 
-            <p>Address: Sylhet, Bangladesh</p>
-            <p> Phone: +88 0123456789 </p>
+            <p>Address: Ojo, Lagos</p>
+            <p> Phone: +88 ******* </p>
             <p>Email: example@gmail.com</p>
           </Col>
         </Row>
