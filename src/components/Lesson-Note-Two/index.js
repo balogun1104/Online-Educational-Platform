a018bcefe7d9489d6e1f5b0982129976990eac3f
Balogun Razak <razak2free@gmail.com>
2023-03-23 15:26:25 +0100

    changes

---
+++ src/components/Lesson-Note-Two/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const LessonNoteTwo = ({nextSept, prevStep}) => {
+  return (
+   <div className='lsnTwoWrapper'>
+    <div className='lsnTwo'>
+    <h2>Behavioural Objectives: At the end of the lesson, student should be able to </h2>
+    </div>
+   </div>
+   
+  )
+}
+
+export default LessonNoteTwo
---
+++ src/components/Lesson-Note/index.js
@@ -0,0 +1,62 @@
+import React , {useState} from 'react'
+import LessonNoteOne from '../Lesson-Note-One';
+import LessonNoteThree from '../Lesson-Note-Three';
+import LessonNoteTwo from '../Lesson-Note-Two';
+import './styles.css';
+
+const LessonNote = ({currentModal,
+    setName,
+    setModalName,
+    currentSellModal}) => {
+
+        const [step, setStep] = useState(1);
+        const [modal, setModal] = useState(false);
+
+    const handleModal = () => {
+        setModal((prev) => !prev);
+      };
+    
+      const nextStep = () => {
+        setStep(step + 1);
+      };
+
+      const prevStep = () => {
+        setStep(nextStep - 1)
+      }
+
+      const render = () => {
+        switch (step) {
+          case 1:
+            return <LessonNoteOne nextStep={nextStep} prevStep={prevStep}/>;
+    
+          case 2:
+            return <LessonNoteTwo nextStep={nextStep} prevStep={prevStep}/>;
+    
+          case 3:
+            return <LessonNoteThree nextStep={nextStep} prevStep={prevStep}/>;
+    
+          default:
+            return <LessonNoteOne />;
+        }
+      };
+
+      
+  return (
+    // <div className='lessonNoteWrapper'>
+    //     <div className='lessonNote'>
+    //         <div className='lessonNoteInner'>
+    //             <h1>Lesson Plan</h1>
+    //         </div>
+    //     </div>
+    // </div>
+
+    <>
+    <div>
+        <h1>Lesson Plan</h1>
+        {render()}
+    </div>
+    </>
+  )
+}
+
+export default LessonNote
